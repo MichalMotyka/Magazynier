bc7ea98bbb1e425524cfad9a2fe8d5d7864c110e
Prodigiq <86515008+Prodigiq@users.noreply.github.com>
2021-09-29 20:56:18 +0200

    0.3
    Wersja 0.3 dodająca konfiguracje jednostek miary

--- Magazynier/src/Tester.java
+++ Magazynier/src/Tester.java
@@ -1,17 +1,20 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Tester {
 
-	public static void main(String[] args) throws SQLException {
-		Login log = new Login();
-		if(log.isZalog("Admin","Admin")) {
-			System.out.println("Zalogowano");
-		}else {
-			System.out.println("Nie zalogowano");
+	public static void main(String[] args) throws SQLException, ParseException {
+		Weryfikator wer = new Weryfikator();
+		Date data = new SimpleDateFormat("yyyy-MM-dd").parse("2020-05-20");
+		Date dataD = new SimpleDateFormat("yyyy-MM-dd").parse("2020-05-19");
+		if(wer.CompareTwoDate(dataD, data)) {
+			System.out.println("Wiêksza jest data"+ data);
+		}else{
+			System.out.println("Wiêksza jest data"+ dataD);
 		}
 	}
 
